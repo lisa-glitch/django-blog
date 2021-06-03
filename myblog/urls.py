1d8a63db3dad890f31badeb40f2afc4394a3731a
Lisa <lisaoloo001@gmail.com>
2021-06-03 13:56:37 +0300

    add templates and views

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
+from myblog.views import CategoryDelete, CategoryList, MyView, CategoryDetail, CategoryCreate, CategoryUpdate
+
 
 urlpatterns=[
     path("", views.home, name='home'),
+    path('view/',MyView.as_view()),
+    path('categories',CategoryList.as_view(), name='category_list'),
+    path('category/detail/<pk>/', CategoryDetail.as_view(),name='category_detail'),
+    path('create/category', CategoryCreate.as_view(), name='category_create'),
+    path('category/update/<pk>/', CategoryUpdate.as_view(), name='category_update'),
+    path('category/delete/<pk>/',CategoryDelete.as_view(), name='category_delete'),
+
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Category
+from django.views import View
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView,DeleteView
+from django.urls import reverse
+
 
 # Create your views here.
 def home(request):
@@ -10,6 +16,34 @@ def home(request):
     #add dictionary during initialization
     context["dataset"]= Category.objects.all()
     return render(request, "home.html", context)
+
+class MyView(View):
+      def get(self, request):
+          return HttpResponse("My first class based views")
+
+class CategoryList(ListView):
+    model=Category
+
+class CategoryDetail(DetailView):
+    model=Category
+
+class CategoryCreate(CreateView):
+    model=Category
+    #specify the fields to be displayed
+    fields=['name']
+    #function to redirect users
+    def get_success_url(self):
+        return reverse('category_list')
+
+class CategoryUpdate(UpdateView):
+    model=Category
+    fields=['name']
+    success_url="/categories"
+
+class CategoryDelete(DeleteView):
+    model=Category
+
+    success_url="/categories"
 
 
 
